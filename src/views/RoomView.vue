3941788cb273123d3cbcc4d9b865edfa3cdf9463
Jacek Bujalski <jacekbujalski@yahoo.com>
2022-04-06 23:55:18 +0200

    Added Area and Room Views

---
+++ src/views/RoomView.vue
@@ -0,0 +1,16 @@
+<template>
+    <rooms/>
+</template>
+
+<script>
+import Rooms from "@/components/Rooms";
+
+export default {
+    name: "Room",
+    components: {Rooms},
+}
+</script>
+
+<style scoped>
+
+</style>
