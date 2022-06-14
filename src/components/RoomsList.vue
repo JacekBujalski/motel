c0bab91ac16b5a58f4db871ec25ca75b81b3cbb2
Jacek Bujalski <jacekbujalski@yahoo.com>
2022-06-14 23:12:16 +0200

    replaced placeholders

--- src/components/RoomsList.vue
+++ src/components/RoomsList.vue
@@ -8,14 +8,16 @@
                 Pokoje
             </div>
             <v-row v-for="room in rooms" :key="room.title">
-                <v-col align-center cols="12" sm="4">
+                <v-col align-center cols="12" sm="5">
                     <v-img class="rounded-xl" contain
-                           src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
+                           v-bind:lazy-src="require('../assets/' + room.lazyName)"
+                           v-bind:src="require('../assets/' + room.fileName)">
+
                     </v-img>
                 </v-col>
                 <v-col class="px-10"
                        cols="12"
-                       sm="8">
+                       sm="7">
                     <h3 class="grey--text">{{ room.title }}</h3>
                     <p class="grey--text">
                         {{ room.description }}
@@ -52,8 +54,8 @@ export default {
             {
                 id: 1,
                 title: "Pokój dwuosobowy standard",
-                img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolorum,quibusdam eum pariatur, laboriosam quia provident placeat corrupti repellat odio at. Hic commodi .",
+                fileName: 'p2s1.jpg', lazyName: 'p2s1lazy.jpg',
+                description: 'Posiada nowoczesną łazienkę, lodówkę, TV oraz czajnik elektryczny. Powierzchnia pokoju wynosi 14 m². ',
                 offers: [
                     {
                         icon: "mdi-bed-double",
@@ -75,14 +77,19 @@ export default {
                         icon: "mdi-kettle",
                         subtitle: "Czajnik w pokoju",
                     },
+                    {
+                        icon: "mdi-eye-outline",
+                        subtitle: "Widok na dziedziniec",
+                    },
 
                 ]
             },
             {
                 id: 2,
                 title: "Pokój dwuosobowy delux z widokiem na morze",
-                img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolorum,quibusdam eum pariatur, laboriosam quia provident placeat corrupti repellat odio at. Hic commodi .",
+                fileName: 'p2d5.jpg',
+                lazyName: 'p2d5lazy.jpg',
+                description: 'Posiada nowoczesną łazienkę, lodówkę, TV oraz czajnik elektryczny. Powierzchnia pokoju wynosi 18,5 m². ',
                 offers: [
                     {
                         icon: "mdi-bed-double",
@@ -110,8 +117,9 @@ export default {
             {
                 id: 3,
                 title: "Pokój rodzinny z widokiem na morze",
-                img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolorum,quibusdam eum pariatur, laboriosam quia provident placeat corrupti repellat odio at. Hic commodi .",
+                fileName: 'pr1.jpg',
+                lazyName: 'pr1lazy.jpg',
+                description: 'Posiada nowoczesną łazienkę, lodówkę, TV oraz czajnik elektryczny. Powierzchnia pokoju wynosi 25 m². ',
                 offers: [
                     {
                         icon: "mdi-bed-double",
@@ -142,8 +150,9 @@ export default {
             {
                 id: 4,
                 title: "Pokój trzyosobowy standard",
-                img: "https://cdn.vuetifyjs.com/images/cards/docks.jpg",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis consequuntur dolorum,quibusdam eum pariatur, laboriosam quia provident placeat corrupti repellat odio at. Hic commodi .",
+                fileName: 'p34.jpg',
+                lazyName: 'p34lazy.jpg',
+                description: 'Posiada nowoczesną łazienkę, lodówkę, TV oraz czajnik elektryczny. Powierzchnia pokoju wynosi 19,5 m². ',
                 offers: [
                     {
                         icon: "mdi-bed",
@@ -166,8 +175,8 @@ export default {
                         subtitle: "Lodówka w pokoju",
                     },
                     {
-                        icon: "mdi-kettle",
-                        subtitle: "Czajnik w pokoju",
+                        icon: "mdi-eye-outline",
+                        subtitle: "Widok na dziedziniec",
                     },
                 ]
             },
